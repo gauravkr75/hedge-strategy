a472266f84b13a0eef2019a4d6aab745782c4f96
Gaurav <gauravkr75@gmail.com>
2021-04-11 02:44:09 +0000

    Changes for cloud deployment

--- srv/server.js
+++ srv/server.js
@@ -2,7 +2,7 @@
 
 const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
-
-cds.on("bootstrap", app => app.use(proxy()));
+const port = process.env.PORT || 4004;
+cds.on("bootstrap", app => app.use(proxy({ port: port })));
 
 module.exports = cds.server;
