7e9fe8f098adac49ff25041e294efa1b046afe5f
Gaurav Kumar <gaurav.kumar04@sap.com>
2021-08-12 02:31:19 +0000

    Deleting duplicate test apps

--- app/hedging/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend("hedging.Component", {
-        metadata: {
-            manifest: "json"
-        }
-    });
-});
